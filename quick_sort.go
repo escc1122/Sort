12b4b0c9911fc6b8a1efd421243590ab1c962f36
escc1122 <escc1122@gamil.com>
2022-08-01 16:21:03 +0800

    Add Hoare Quick Sort

--- quick_sort.go
+++ quick_sort.go
@@ -30,10 +30,53 @@ func partition(array []int, left int, right int) int {
 	return newPivot
 }
 
+//https://www.techiedelight.com/zh-tw/quick-sort-using-hoares-partitioning-scheme/
+//https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/645577/golang-with-lomutohoare-partition-scheme
+//Hoare
+func quickSortHoare(array []int, left int, right int) {
+	if left < right {
+		newPivot := partitionHoare(array, left, right)
+		quickSortHoare(array, left, newPivot-1)
+		quickSortHoare(array, newPivot+1, right)
+	}
+}
+
+func partitionHoare(array []int, left int, right int) int {
+	pivotValue := array[left]
+	i, j := left, right
+	for {
+		for array[i] < pivotValue {
+			i++
+		}
+
+		for array[j] > pivotValue {
+			j--
+		}
+
+		if i >= j {
+			return j
+		}
+		swap(array, i, j)
+		i++
+		j--
+	}
+}
+
+func swap(array []int, a int, b int) {
+	array[a], array[b] = array[b], array[a]
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
 	test := rand.Perm(30)
+	testHoare := make([]int, len(test))
+	copy(testHoare, test)
 	fmt.Println(test)
+
 	quickSort(test, 0, len(test)-1)
 	fmt.Println(test)
+
+	quickSortHoare(testHoare, 0, len(testHoare)-1)
+	fmt.Println(testHoare)
+
 }
